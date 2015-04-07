bfa6e7f3b5a46afcceb6f5cb0517ffe82734ecb3
Tommyluo <tluo12321@yahoo.com>
2015-04-07 16:43:44 -0700

    exercises

--- client/index.js
+++ client/index.js
@@ -1,2 +1,69 @@
 'use strict';
-console.log('apollo -> online');
+ $(document).ready(init);
+
+ function init() {
+  //  $('#go').click(go);
+  //  $('#calc').click(calc);
+
+  $('#clear').click(clear);
+  $('.article').click(setText);
+  $('.noun').click(setText);
+  $('.adj').click(setText);
+  $('.verb').click(setText);
+ }
+
+ function clear() {
+   $('#sentence').text('');
+ }
+
+ function setText() {
+   var oldText = $('#sentence').text();
+   var text = $(this).text();
+   var newText = oldText + ' ' + text;
+   $('#sentence').text(newText);
+ }
+
+
+// function go() {
+//   var numbers = $('#numbers').val();
+//   numbers = numbers.split(',');
+//   numbers = numbers.map(function(n) {
+//     return n * 1;
+//   });
+//
+//   var sum = numbers.reduce(function(old, curr) {
+//     return old + curr;
+//   });
+//
+//   $('#sum').text(sum);
+//
+//   var products = numbers.reduce(function(old, curr) {
+//     return old * curr;
+//   });
+//
+//   $('#product').text(products);
+// }
+
+
+// function calc() {
+//   var x = parseInt($('#x').val());
+//   var op = $('#op').val();
+//   var y = parseInt($('#y').val());
+//   var answer;
+//
+//   switch (op) {
+//     case '+':
+//       answer = x + y;
+//       break;
+//     case '-':
+//       answer = x - y;
+//       break;
+//     case '*':
+//       answer = x * y;
+//       break;
+//     case '/':
+//       answer = x / y;
+//   }
+//
+//   $('#answer').text(answer);
+// }
